b97a97de7792efa69130311a7e9d4415a43d1e78
renan <refilho@gmail.com>
2023-12-15 14:21:26 -0300

    teste-processamento

---
+++ udemy-curso/teste-processamento/src/Main.java
@@ -0,0 +1,12 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int x, y;
+        x = 5;
+        y = 2 * x;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
